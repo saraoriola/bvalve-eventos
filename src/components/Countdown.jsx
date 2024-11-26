9d1f04a1e0acd22224a09fbf52f5568ab2390f1e
saraoriola <saraoriola@outlook.com>
2024-11-26 18:35:58 +0100

    Prueba ok

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import starIcon from "../assets/img/estrella.png";
 import TimeUnit from "./TimeUnit";
+import './styles/Countdown.css'
 
 const Countdown = ({ timeLeft }) => (
   <div className="whitebox">
     <div className="countdown-wrapper">
-    <img src={starIcon} alt="Estrella" className="countdown-icon" />
-    <div className="countdown-timer">
-      <TimeUnit label="Días" value={timeLeft.days} />
-      <TimeUnit label="Horas" value={timeLeft.hours} />
-      <TimeUnit label="Minutos" value={timeLeft.minutes} />
-      <TimeUnit label="Segundos" value={timeLeft.seconds} />
+      <img src={starIcon} alt="Estrella" className="countdown-icon" />
+      <div className="countdown-timer">
+        <TimeUnit label="Días" value={timeLeft.days} />
+        <TimeUnit label="Horas" value={timeLeft.hours} />
+        <TimeUnit label="Minutos" value={timeLeft.minutes} />
+        <TimeUnit label="Segundos" value={timeLeft.seconds} />
+      </div>
+      <img src={starIcon} alt="Estrella" className="countdown-icon" />
     </div>
-    <img src={starIcon} alt="Estrella" className="countdown-icon" />
   </div>
-  </div>
-    
 );
 
 export default Countdown;
